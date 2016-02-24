8eb2db5173d4510f8a7a817f8a03aefcd7eb09c6
hieunt <hieu.nguyen@taplifeapp.com>
2016-02-24 10:41:10 +0700

    temple run pro

---
+++ Assets/2_Scripts/Prototypes/MovingGround.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingGround : MonoBehaviour {
+	Renderer mRenderer;
+	// Use this for initialization
+	void Start () {
+		mRenderer = GetComponent<Renderer>();
+	}
+	//Material texture offset rate
+	float speed = .5f;
+
+	//Offset the material texture at a constant rate
+	void Update () {
+		float offset = Time.time * speed;                             
+		mRenderer.material.mainTextureOffset = new Vector2(0, -offset); 
+
+	}
+}
---
+++ Assets/2_Scripts/Prototypes/Vehicle.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vehicle : MonoBehaviour
+{
+	int currentPosition;	// -1, 0, 1
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// get horizontal input
+		int hInput =  (int) Input.GetAxisRaw("Horizontal");
+		switch (hInput) {
+		case 0:
+			break;
+		case 1:
+			TurnRight();
+			break;
+		case -1:
+			TurnLeft();
+			break;
+		}
+	}
+
+	void TurnLeft ()
+	{
+		if (currentPosition != -1)
+			currentPosition--;
+		LeanTween.move(gameObject, new Vector3 (transform.position.x - 1f, transform.position.y, transform.position.z), .2f);
+//		Debug.Log(currentPosition);
+	}
+
+	void TurnRight () 
+	{
+		if (currentPosition != 1)
+			currentPosition++;
+		LeanTween.move(gameObject, new Vector3 (transform.position.x + 1f, transform.position.y, transform.position.z), .2f);
+//		Debug.Log(currentPosition);
+	}
+
+}
