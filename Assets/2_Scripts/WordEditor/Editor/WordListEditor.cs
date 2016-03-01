418a01ae5d32669bb8e76956457dce6dac388382
Khanh Hung <khanhhung@Luongs-iMac.local>
2016-03-01 15:04:09 +0700

    Add group button

---
+++ Assets/2_Scripts/WordEditor/Editor/WordListEditor.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(WordListHandler))]
+public class WordListEditor : Editor {
+
+    private WordListHandler _wordListHandler;
+
+    void OnEnable()
+    {
+        _wordListHandler = (WordListHandler)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        GUILayout.Space(10f);
+        serializedObject.Update();
+        GUILayout.Label("WORD LIST CONFIGURATION");
+        GUILayout.Space(5f);
+
+        DrawProperty("_inputWordFileName", 5f);
+
+        GUILayout.Space(5f);
+        if(GUILayout.Button("Read Word List"))
+        {
+            _wordListHandler.ReadWordList();
+        }
+
+        DrawArrayProperty("_letterGroups", 5f);
+        DrawProperty("_filterMode", 5f);
+
+        // Export Menu
+        GUILayout.Space(10f);
+        GUILayout.Label("SAVE OPTION");
+        GUILayout.Space(5f);
+        SerializedProperty directoryPath = serializedObject.FindProperty("_directoryPath");
+        if (string.IsNullOrEmpty(directoryPath.stringValue))
+        {
+            EditorGUILayout.HelpBox("Please select a directory where the level will be saved.", MessageType.Error, true);
+        }
+        else
+        {
+            if (!System.IO.Directory.Exists(directoryPath.stringValue))
+            {
+                directoryPath.stringValue = string.Empty;
+            }
+        }
+
+        if (GUILayout.Button(!string.IsNullOrEmpty(directoryPath.stringValue) ? (directoryPath.stringValue.Contains("Assets") ? directoryPath.stringValue.Substring(directoryPath.stringValue.IndexOf("Assets")) : directoryPath.stringValue) : "Select directory", "PreDropDown"))
+        {
+            string path = EditorUtility.OpenFolderPanel("Select directory, where the level will be saved.", "", "");
+            if (!string.IsNullOrEmpty(path))
+            {
+                directoryPath.stringValue = path;
+            }
+        }
+
+        DrawProperty("_outputFileName", 5f);
+
+
+        GUILayout.Space(10f);
+        if (GUILayout.Button("Generate Word List"))
+        {
+            _wordListHandler.CreateWordList();
+        }
+
+		GUILayout.Space(10f);
+		if (GUILayout.Button("Generate Group Length Word List"))
+		{
+			_wordListHandler.CreateGroupWordList();
+		}
+
+        serializedObject.ApplyModifiedProperties();
+        bool guiEnabled = GUI.enabled;
+
+        GUI.enabled = guiEnabled;
+
+        if (GUI.changed)
+            EditorUtility.SetDirty(_wordListHandler);
+    }
+
+    private SerializedProperty DrawArrayProperty(string propertyName, float space)
+    {
+        GUILayout.Space(space);
+        return DrawProperty(propertyName, true, true);
+    }
+
+    private SerializedProperty DrawProperty(string propertyName, float space, bool isArray = false)
+    {
+        GUILayout.Space(space);
+        return DrawProperty(propertyName, false, isArray);
+    }
+
+    private SerializedProperty DrawProperty(string propertyName, bool isArray = false)
+    {
+        return DrawProperty(propertyName, false, isArray);
+    }
+
+    private SerializedProperty DrawProperty(string propertyName, bool insert, bool isArray = false)
+    {
+        SerializedProperty property = serializedObject.FindProperty(propertyName);
+        if (insert)
+        {
+            EditorGUI.indentLevel += 1;
+        }
+        EditorGUILayout.PropertyField(property, isArray);
+
+        if (insert)
+        {
+            EditorGUI.indentLevel -= 1;
+        }
+        return property;
+    }
+}
---
+++ Assets/2_Scripts/WordEditor/WordListHandler.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+
+public class WordListHandler : MonoBehaviour
+{
+	[SerializeField]
+	private string
+		_directoryPath;
+	[SerializeField]
+	private string
+		_outputFileName;
+	[SerializeField]
+	private string
+		_inputWordFileName;
+	[SerializeField]
+	private string[]
+		_letterGroups;
+
+	public enum FilterMode
+	{
+		REPEATED,
+		NO_REPEATED
+	}
+
+	[SerializeField]
+	private FilterMode
+		_filterMode;
+	private List<string> _dict;
+	private Dictionary<int, List<string>> groupLengthResult = new Dictionary<int, List<string>> ();
+
+	public void ReadWordList ()
+	{
+		_dict = new List<string> ();
+
+		TextAsset wordlist = Resources.Load<TextAsset> (_inputWordFileName);
+		JSONObject jsonObject = new JSONObject (wordlist.text);
+
+		AccessData (jsonObject);
+	}
+
+	public void CreateWordList ()
+	{
+		JSONObject j = new JSONObject (JSONObject.Type.OBJECT);
+		j.AddField("");
+		foreach (var key in groupLengthResult.Keys) {
+			JSONObject arr = new JSONObject (JSONObject.Type.ARRAY);
+			j.AddField ("" + key, arr);
+			
+			foreach (string word in groupLengthResult[key])
+				arr.Add (word);
+			
+			string encodedJson = j.Print ();
+			
+			WriteTextFile (encodedJson, _outputFileName, _directoryPath);
+		}
+	}
+
+	public void CreateGroupWordList ()
+	{
+		List<string> result = FindWordWithLetters (_letterGroups [0]);
+		
+		
+		JSONObject j = new JSONObject (JSONObject.Type.OBJECT);
+
+		JSONObject arr = new JSONObject (JSONObject.Type.ARRAY);
+		j.AddField ("wordlist", arr);
+			
+		foreach (string word in result)
+			arr.Add (word);
+			
+		string encodedJson = j.Print ();
+			
+		WriteTextFile (encodedJson, _outputFileName, _directoryPath);
+	}
+
+    #region PRIVATE METHOD
+
+	private void GroupWordByLength (string word)
+	{
+		if (groupLengthResult.ContainsKey (word.Length))
+			groupLengthResult [word.Length].Add (word);
+		else
+			groupLengthResult.Add (word.Length, new List<string> ());
+	}
+
+	private void WriteTextFile (string data, string fileName, string directoryPath)
+	{
+		var sr = File.CreateText (directoryPath + "/" + fileName);
+		sr.Write (data);
+		sr.Close ();
+	}
+
+	private List<string> FindWordWithLetters (string letters)
+	{
+		List<string> listWithWord = new List<string> ();
+
+		foreach (string word in _dict) {
+			if (IsContructedByLetters (word, letters)) {
+				listWithWord.Add (word);
+				GroupWordByLength (word);
+			}
+		}
+
+		return listWithWord;
+	}
+
+	private bool IsContructedByLetters (string word, string letters)
+	{
+		bool isSameAsGroup = false;
+
+		for (int i = 0; i < word.Length; i++) {
+			for (int k = 0; k < letters.Length; k++) {
+				if (word [i] == letters [k]) {
+					isSameAsGroup = true;
+					break;
+				} else
+					isSameAsGroup = false;
+			}
+
+			if (!isSameAsGroup)
+				return false;
+		}
+
+		return true;
+	}
+
+	private void AccessData (JSONObject obj)
+	{
+		switch (obj.type) {
+		case JSONObject.Type.OBJECT:
+			for (int i = 0; i < obj.list.Count; i++) {
+				string key = (string)obj.keys [i];
+				JSONObject j = (JSONObject)obj.list [i];
+				AccessData (j);
+			}
+			break;
+		case JSONObject.Type.ARRAY:
+			foreach (JSONObject j in obj.list) {
+				AccessData (j);
+			}
+			break;
+		case JSONObject.Type.STRING:
+			if (obj.str.Length > 2)
+				_dict.Add (obj.str);
+			break;
+		case JSONObject.Type.NUMBER:
+			break;
+		case JSONObject.Type.BOOL:
+			break;
+		case JSONObject.Type.NULL:
+			break;
+		}
+	}
+	#endregion
+	
+}
+
