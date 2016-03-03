c6b1eb48f3cf757597f4c91cdf021ce72a7d3680
hieunt <hieu.nguyen@taplifeapp.com>
2016-03-03 17:42:32 +0700

    add letter prefabs

---
+++ Assets/2_Scripts/Singleton/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class AccountController : UnitySingletonPersistent<AccountController> {
+
+	// account data stored
+	// name
+	// score
+	// strong word list (optional)
+	// car unlocked
+	// current selected car 
+
+	public class AccountData {
+		public string username;
+		public int score;
+		public List<string> stringWords;
+	}
+
+	#region public members
+	#endregion public members
+
+	#region private members
+	#endregion private members
+
+	#region Mono
+	#endregion Mono
+
+	#region public functions
+	#endregion public functions
+
+	#region private functions
+	#endregion private functions
+
+}
