27e55a95f1d7e73ade0f062df4febc3a9365c657
hieunt <hieu.nguyen@taplifeapp.com>
2016-03-03 09:38:42 +0700

    car game controller with playmaker

---
+++ Assets/2_Scripts/Prototypes/CarGameController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarGameController : MonoBehaviour {
+
+	#region public members
+
+	#endregion public members
+
+	#region private members
+	#endregion private members
+
+	#region Mono
+	#endregion Mono
+
+	#region public functions
+
+	#endregion public functions
+
+	#region private functions
+	void Init () {
+		GetWordData ();
+		GetCarData ();
+		GenerateLetter ();
+		GenerateObstacles ();
+	}
+
+	void GetWordData () {
+
+	}
+
+	void GetCarData () {
+
+	}
+
+	void GenerateLetter () {
+
+	}
+
+	void GenerateObstacles () {
+
+	}
+
+	#endregion private functions
+
+}
