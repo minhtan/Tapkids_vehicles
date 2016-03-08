56a435191d9a2b1b2702775a726ca2e5d7084a4c
Khanh Hung <khanhhung@Luongs-iMac.local>
2016-03-08 10:04:38 +0700

    Add SceneController

---
+++ Assets/2_Scripts/Singleton/SceneController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+public class SceneController : UnitySingletonPersistent<SceneController>
+{
+	#region VAR
+
+	[SerializeField]
+	private SceneContainer[] _sceneGroup;
+
+	public enum SceneID{
+		MENU, LOADING
+	}
+	#endregion
+
+	public static UnityAction OnStartLoading;
+	public static UnityAction OnEndLoading;
+	public static UnityAction<float> OnLoadingScene;
+
+	public override void Awake ()
+	{
+		base.Awake ();
+		//InitSceneGroup ();
+	}
+
+	public void LoadingSceneAsync (SceneID id)
+	{
+		StartCoroutine (LoadingOperation(id, 0f));
+	}
+
+	private IEnumerator LoadingOperation(SceneID id, float delay = 0f){
+		if (delay > 0f)
+			yield return new WaitForSeconds (delay);
+
+		if (OnStartLoading != null)
+			OnStartLoading ();
+		
+		AsyncOperation async = SceneManager.LoadSceneAsync ((int)id);
+	
+		async.allowSceneActivation = false;
+		
+		while(!async.isDone)
+		{
+			if (OnLoadingScene != null)
+				OnLoadingScene (async.progress);	
+
+			if (async.progress == 0.9f) {
+				if(OnEndLoading != null)
+					OnEndLoading ();
+				async.allowSceneActivation = true;
+				yield break;
+			}
+			
+			yield return null;
+		}
+	}
+
+	#region PRIVATE METHOD
+	private void InitSceneGroup ()
+	{
+		_sceneGroup = new SceneContainer[SceneManager.sceneCount];
+		for (int i = 0; i < SceneManager.sceneCount; i++) {
+			SceneContainer container = new SceneContainer ();
+
+			container.SceneData = SceneManager.GetSceneAt (i);
+			_sceneGroup [i] = container;
+		}
+	}
+
+	private Scene GetSceneByID(SceneID id)
+	{
+		for(int i = 0; i< _sceneGroup.Length; i++)
+		{
+			if (id == _sceneGroup [i].ID)
+				return _sceneGroup [i].SceneData;
+		}
+		return _sceneGroup[0].SceneData;
+	}
+	#endregion
+
+	[System.Serializable]
+	public class SceneContainer : System.Object
+	{
+		[SerializeField]
+		private SceneID _sceneID;
+
+		public SceneID ID{ set { _sceneID = value; } get { return _sceneID; } }
+
+		[SerializeField]
+		private Scene _scene;
+
+		public Scene SceneData{ set { _scene = value; } get { return _scene; } }
+	}
+}
