e1dfb0e1d6600eed0db8a6c87041c693d1db60c4
hieunt <hieu.nguyen@taplifeapp.com>
2016-03-11 14:59:51 +0700

    structre player data  manager

---
+++ Assets/2_Scripts/Account/Player.cs
@@ -0,0 +1,14 @@
+﻿
+[System.Serializable]
+public class Player {
+	public string name;
+	public Vehicle[] unlockeds;
+	public int currentVehicle;
+}
+
+[System.Serializable]
+
+public class Vehicle {
+	public int id;
+	public string name;
+}
