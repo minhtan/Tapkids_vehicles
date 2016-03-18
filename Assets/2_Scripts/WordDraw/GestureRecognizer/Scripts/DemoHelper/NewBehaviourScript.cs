305f485ebd19243def2a4d97d842b294e2abd249
Tan <m.tan2007@gmail.com>
2016-03-18 12:02:17 +0700

    Add Demo Word Draw Scene

---
+++ Assets/2_Scripts/WordDraw/GestureRecognizer/Scripts/DemoHelper/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	#region Vars
+
+	#endregion
+
+	#region Mono
+	void Awake(){
+
+	}
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+	#endregion
+
+
+}
---
+++ Assets/2_Scripts/WordDraw/GestureRecognizer/Scripts/DemoHelper/TemplateAdder.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using PDollarGestureRecognizer;
+
+public class TemplateAdder : MonoBehaviour {
+	public LeanGestureRecognizer _recognizer;
+	public InputField _inputField;
+
+	private Result _result;
+
+	void OnEnable()
+	{
+		LeanGestureRecognizer.OnGestureDetected += OnResult;
+	}
+
+	void OnDisable()
+	{
+		LeanGestureRecognizer.OnGestureDetected -= OnResult;
+	}
+
+	void OnResult(Result result)
+	{
+		_result = result;
+	}
+
+	public void AddTemplate()
+	{
+		_recognizer.SaveCurrentGesture (_inputField.text);
+	}
+}
