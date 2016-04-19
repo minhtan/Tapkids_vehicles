cf90ac5892d9bc8329ee0093f15ce5d9c7e677ef
hieunt <hieu.nguyen@taplifeapp.com>
2016-04-19 11:54:26 +0700

    implement ar controller to car game

---
+++ Assets/2_Scripts/CarGame/Test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ArController.Instance.ToggleAR (true);
+		ArController.Instance.SetCenterMode (false);
+		ArController.Instance.SetArMaxStimTargets (1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
