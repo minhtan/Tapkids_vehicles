9d4c553d3db583ddd0feef4da800a4b52a62ec6e
hieunt <hieu.nguyen@taplifeapp.com>
2016-05-05 17:52:09 +0700

    edit persistent gui + player credit update feature

--- Assets/2_Scripts/CarGame/GUI/Buttons/PauseButton.cs
+++ Assets/2_Scripts/CarGame/GUI/Buttons/PauseButton.cs
@@ -8,26 +8,41 @@ public class PauseButton : MonoBehaviour {
 	#endregion public members
 
 	#region private members
+	private CanvasGroup mCanvasGroup;
 	private Button pauseButton;
 	#endregion private members
 
 	#region Mono
+	void OnEnable () {
+		Messenger.AddListener <bool> (EventManager.GUI.TOGGLE_INGAME.ToString (), OnToggleInGamePanel);
+	}
+
 	void Start () {
+		mCanvasGroup = GetComponent <CanvasGroup> ();
 		pauseButton = GetComponent <Button> ();
 		if(pauseButton != null) {
 			pauseButton.onClick.AddListener (delegate {
-				//CarGameEventController.OnPauseGame (true);
-//				CarGameEventController.OnTogglePanel ("pause");
 				Messenger.Broadcast<bool> (EventManager.GameState.PAUSE.ToString(), true);
 			});
 		}
 	}
+
+
+	void OnDisable () {
+		Messenger.RemoveListener <bool> (EventManager.GUI.TOGGLE_INGAME.ToString (), OnToggleInGamePanel);
+	}
+
 	#endregion Mono
 
 	#region public functions
 	#endregion public functions
 
 	#region private functions
+	private void OnToggleInGamePanel (bool _isToggled) {
+		mCanvasGroup.alpha = _isToggled ? 1f : 0f;
+		mCanvasGroup.interactable = _isToggled ? true : false;
+		mCanvasGroup.blocksRaycasts = _isToggled ? true : false;
+	}
 	#endregion private functions
 
 }
