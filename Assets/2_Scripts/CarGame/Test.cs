18e6fe4f24930b7ab1196917129f0b7560ca1543
hieunt <hieu.nguyen@taplifeapp.com>
2016-08-08 17:34:35 +0700

    edit models

--- Assets/2_Scripts/CarGame/Test.cs
+++ Assets/2_Scripts/CarGame/Test.cs
@@ -3,38 +3,18 @@ using System.Collections;
 using System;
 using System.Linq;
 
-public class Test : MonoBehaviour {
+public class Test : StateMachineBehaviour {
 
-	void Start(){
-		StartCoroutine(RunVehicle(gameObject.transform, GameObject.FindObjectOfType<BezierSpline>(), () => {
-			
-		}));
+
+	public bool active;
+	public override void OnStateEnter (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		Debug.Log ("Start " + layerIndex);
 	}
 
-	IEnumerator RunVehicle (Transform _trans, BezierSpline _spline, System.Action _callback = null, bool _isGoingForward = true, float _duration = 1f) {
-		float progress = _isGoingForward ? 0 : 1;
-		while (true) {
-			if (_isGoingForward) {
-				progress += Time.deltaTime / _duration;
-				if (progress >= 1f) {
-					if (_callback != null)
-						_callback ();
-					yield break;
-				}
-			}else{
-				progress -= Time.deltaTime / _duration;
-				if (progress < 0f) {
-					if (_callback != null)
-						_callback ();
-					yield break;
-				}
-			}
-
-			Vector3 position = _spline.GetPoint(progress);
-			_trans.localPosition = position;
-			_trans.LookAt(position + _spline.GetDirection(progress));
-			yield return null;
-		}
+	public override void OnStateExit (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		
 	}
 
 }
